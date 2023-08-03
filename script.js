8be7ae226669a6d0f13acd31a61d1f9245603c02
ALI HAIDER <ah0062527@gmail.com>
2023-08-03 09:19:17 +0500

    added the Javascript file for ProductLandingPage.html

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript to handle the CTA button click
+document.addEventListener('DOMContentLoaded', function () {
+    const ctaButton = document.querySelector('.cta-btn');
+
+    ctaButton.addEventListener('click', function () {
+        // Replace this with the desired action for the CTA button click, such as redirecting to a purchase page or displaying a modal form.
+        alert('Thank you for your interest in our product!');
+    });
+});
+
+
+
+
